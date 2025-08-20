ea6b71f28e2b71a70ba31e048de3a91aea22aebd
andre-savedra <andre_savedra@hotmail.com>
2025-08-19 22:16:33 -0300

    organized repository, added MangeCoin

--- BACKEND-DJANGO-master/Aula6_Django_Web_Token/programacao/aula1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('', include('djoser.urls')), #adiciona as rotas de gerenciamento de usuarios
-    path('auth/', include('djoser.urls.authtoken')), #adiciona a rota de obter token
-]
