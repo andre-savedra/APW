57e3ac0a7b6bac3135a697f2537da4659318e97f
andre-savedra <andre_savedra@hotmail.com>
2025-09-04 22:05:12 -0300

    mangecoin correction

--- TURMA_B/2-MANGECOIN/app/urls.py
+++ TURMA_B/2-MANGECOIN/app/urls.py
@@ -7,9 +7,10 @@ router = DefaultRouter()
 
 router.register(r'users', CustomUserView)
 router.register(r'tokens', TokenView)
+router.register(r'accounts/tokens', AccountTokenView)
 router.register(r'accounts', AccountView)
-router.register(r'accounts-tokens', AccountTokenView)
 router.register(r'bets', BetView)
 router.register(r'transactions', TransactionView)
 
 urlpatterns = router.urls
+urlpatterns.append(path('bets/try', BetTryView.as_view(), name='bet-try'))
--- TURMA_B/2-MANGECOIN/app/views.py
+++ TURMA_B/2-MANGECOIN/app/views.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.views import APIView
 from .serializers import *
 from .models import *
+from random import randint
+from rest_framework.response import Response
 
 class CustomUserView(ModelViewSet):
     queryset = CustomUser.objects.all()
@@ -14,7 +16,7 @@ class AccountView(ModelViewSet):
 class TokenView(ModelViewSet):
     queryset = Token.objects.all()
     serializer_class = TokenSerializer
-
+    
 class AccountTokenView(ModelViewSet):
     queryset = AccountToken.objects.all()
     serializer_class = AccountTokenSerializer
@@ -26,3 +28,28 @@ class TransactionView(ModelViewSet):
 class BetView(ModelViewSet):
     queryset = Bet.objects.all()
     serializer_class = BetSerializer
+
+
+#1 - apenas usuários autenticados podem fazer jogadas OK
+#2 - apenas usuários 18 + podem fazer jogadas
+#3 - apenas usuários com saldo positivo em MANGECOIN podem jogar
+
+class BetTryView(APIView):
+
+    def get(self, request):
+
+        if not request.user.is_authenticated:
+            return Response(status=403,data='Usuário não está autenticado!')
+
+
+        # 3 roletas de 5 imagens (0,1,2,3,4)
+        value1 = randint(0,4)
+        value2 = randint(0,4)
+        value3 = randint(0,4)
+
+
+        return Response(status=200,data={
+            'bet1': value1,
+            'bet2': value2,
+            'bet3': value3
+        })
