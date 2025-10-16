0db6ffe6d18e8ad982608719603f955dea02b3a2
andre-savedra <andre_savedra@hotmail.com>
2025-10-15 22:38:32 -0300

    added terraform

---
+++ TURMA_B/4-TERRAFORM/test-terraform/iac.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "4.48.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  subscription_id = "250ae9c3-6c33-4030-b72a-ed22fce22920"
+  features {
+  }
+}
+
+resource "azurerm_resource_group" "andre_savedra_test_rg" {
+    name = "andre_savedra_test_rg"
+    location = "West Europe"  
+}
+
+
+resource "azurerm_service_plan" "andre_savedra_test_sp" {
+  name = "andre_savedra_test_sp"
+  resource_group_name = azurerm_resource_group.andre_savedra_test_rg.name
+  location = azurerm_resource_group.andre_savedra_test_rg.location
+  sku_name = "S1"
+  os_type = "Windows"
+}
+
+resource "azurerm_windows_web_app" "andre_savedra_test_app" {
+  name = "andre-savedra-test-app"
+  resource_group_name = azurerm_resource_group.andre_savedra_test_rg.name
+  location = azurerm_resource_group.andre_savedra_test_rg.location
+  service_plan_id = azurerm_service_plan.andre_savedra_test_sp.id
+  site_config {
+   always_on = false 
+  }
+}
+
+
+
